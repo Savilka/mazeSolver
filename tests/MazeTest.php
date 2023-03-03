764d80a8235401f117985dc25b392ceb8e1d61dd
Semyon Morgunov <simanmore@yandex.ru>
2023-03-03 13:22:43 +0700

    Добавил файл для тестирования

---
+++ tests/MazeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class MazeTest extends TestCase
+{
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testFindPathCost()
+    {
+
+    }
+}
